10496ef297d893e6aca333c3c9fc761e97c5f3b1
sojw <gravitycage@gmail.com>
2023-09-18 01:34:42 +0900

    Add files via upload

---
+++ job/pull_request_reminder/conf.py
@@ -0,0 +1,13 @@
+config = \
+    {
+        'slack_notification': "on",
+        'repo_list': [
+            # [git token , repo name, slack webhook url]
+            # rps
+            [
+                'ghp_xxxxx',
+                ["y/xxx"],
+                "https://hooks.slack.com/services/xxx/xxx/xxx"
+            ]
+        ]
+    }
